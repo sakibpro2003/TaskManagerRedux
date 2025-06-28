424f1c1a5f7de681817ae339b71fa4cd269252af
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-06-28 12:37:58 +0600

    feat: shadcn + tailwind installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./component/shared/Navbar";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Button>clidkc</Button>
     </>
   );
 }
