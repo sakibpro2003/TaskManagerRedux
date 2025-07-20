ea5978e18b0d026560bf26fc09c062f6f73b646b
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-07-20 16:08:51 +0600

    feat: event interface created

---
+++ src/components/ui/modules/TaskModal.tsx
@@ -0,0 +1,60 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../form";
+import { useForm } from "react-hook-form";
+import { Input } from "../input";
+const TaskModal = () => {
+  const form = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Open Dialog</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel />
+                  <FormControl>
+                    <Input {...field} value={field.value || " "} type="text"></Input>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Save changes</Button>
+          </form>
+        </Form>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default TaskModal;
